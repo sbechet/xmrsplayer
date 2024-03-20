11deac6afddf5eae9fabc3c46d62560fe3ddbadf
Sébastien Béchet <sebastien.bechet@free.fr>
2024-03-20 22:25:11 +0100

    add example for debug

--- examples/rodio_player.rs
+++ examples/rodio_player.rs
@@ -90,10 +90,8 @@ fn rodio_play(module: Arc<Module>, amplification: f32, position: usize, loops: u
             println!("Debug on");
         }
         player_lock.debug(debug);
-        // player_lock.set_mute_channel(0, true);
-        // player_lock.set_mute_channel(1, true);
-        // player_lock.set_mute_channel(2, true);
-        // player_lock.set_mute_channel(3, true);
+        // player_lock.mute_all(true);
+        // player_lock.set_mute_channel(20, false);
         player_lock.set_max_loop_count(loops);
         player_lock.goto(position, 0);
     }
